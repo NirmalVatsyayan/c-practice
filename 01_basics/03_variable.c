0c14c46d003e41a2fadd7d873dee659773d9dd5a
Nirmal Vatsyayan <nirmal.vatsyayan1990@gmail.com>
2017-03-05 22:52:06 +0530

    started revising c

---
+++ 01_basics/03_variable.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<conio.h>
+
+int main(){
+    int age;
+    float weight;
+    char sex;
+
+    age = 27;
+    weight = 65.5;
+    sex = 'M';
+
+
+    printf("Age -> %d || Weight -> %f || Sex -> %c", age, weight, sex);
+
+}
---
+++ 01_basics/04_user_input.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include<conio.h>
+
+int main(){
+
+    int age;
+
+    printf("Enter You Age\n");
+    scanf("%d", &age);
+    printf("Your age is -> %d",age);
+
+}
